6be070964f51a87c27587510d8133987ce315c25
wangqisheng <wqs1639@gmail.com>
2017-05-29 04:42:33 +0800

    YSDK
    添加 ysdk 支付相关接口

--- src/main.py
+++ src/main.py
@@ -11,6 +11,7 @@ import hmac
 import random
 import asyncio
 import aiohttp
+import aiofiles
 from base64 import b64encode, encodebytes
 from hashlib import sha1
 from urllib.parse import urlencode
@@ -42,12 +43,28 @@ async def daily_task():
     while True:
         await asyncio.sleep(1)
 
+@app.listener('before_server_start')
+async def after_server_start(app, loop):
+    "服务启动之前调用, 用以检查 pid"
+    # app.add_task(daily_task())
+    # global client_session
+    # client_session = aiohttp.ClientSession()    
+    pid = ""
+    async with aiofiles.open('pid.txt', 'r') as f:
+        await f.read()
+    print('before')
+
 
 @app.listener('after_server_start')
 async def after_server_start(app, loop):
+    "服务启动后调用"
     # app.add_task(daily_task())
-    global client_session
-    client_session = aiohttp.ClientSession()
+    # global client_session
+    # client_session = aiohttp.ClientSession()    
+    pid = os.getpid()
+    async with aiofiles.open('pid.txt', 'w') as f:
+        await f.write(str(pid))
+
     loop.create_task(daily_task())
 
 
@@ -76,8 +93,12 @@ def server_error(request, exception):
 
 @app.route("/")
 async def test(request):
-    url = app.url_for('hello')
-    return redirect(url)
+    "测试用"
+    # url = app.url_for('hello')
+    url = 'http://localhost:8000/info'
+    rst = await client_session.get(url)
+    ret = await rst.text(encoding='utf8')
+    return html(ret)
 
 
 @app.get('/error')
@@ -156,18 +177,19 @@ async def get_balance_m(req):
     "查询余额接口"
     # https:// ysdk.qq.com/mpay/get_balance_m
     # https://ysdktest.qq.com/mpay/get_balance_m
+    d = req.args['param']
     url = "https://ysdktest.qq.com"
     uri = "/mpay/get_balance_m"
     sig_uri = '/v3/r'
-    data = {}
-    data['openid'] = ""
-    data['openkey'] = ""
-    data['appid'] = ""
-    data['ts'] = ""
-    data['sig'] = ""
-    data['pf'] = ""
-    data['pfkey'] = ""
-    data['zoneid'] = ""
+    params = {}
+    params['openid'] = ""
+    params['openkey'] = ""
+    params['appid'] = ""
+    params['ts'] = ""
+    params['sig'] = ""
+    params['pf'] = ""
+    params['pfkey'] = ""
+    params['zoneid'] = ""
     cookies = {}
     cookies['session_id'] = "open_id"
     cookies['session_id'] = "hy_gameid"
@@ -176,28 +198,136 @@ async def get_balance_m(req):
     cookies['org_loc'] = '/mpay/get_balance_m'
     for k, v in cookies.items():
         cookies[k] = urlencode(v)
-    sig = gen_sign(data, sig_uri, "")
-    data['sig'] = sig
+    sig = gen_sign(params, sig_uri, "")
+    params['sig'] = sig
     url += uri
-    rst = await client_session.get(url)
-    r = await rst.text(encoding='utf8')
-    print(r)
-    return json({})
+    async with aiohttp.ClientSession(cookie=cookies) as session:
+        rst = await session.get(url, params=params)
+        ret = await rst.json()
+        if int(ret['ret']) != 0:
+            print('查询余额失败\n', ret)
+        return json(ret)
+
 
 @app.get('/wzgj/mpay/pay_m')
 async def pay_m(req):
     "扣除游戏币接口"
-    return json({})    
+    # https://ysdk.qq.com/mpay/pay_m
+    # https://ysdktest.qq.com/mpay/pay_m
+    d = req.args['param']
+    url = 'https://ysdktest.qq.com'
+    uri = "/mpay/pay_m"
+    sig_uri = '/v3/r'
+    params = {}
+    params['openid'] = ""
+    params['openkey'] = ""
+    params['appid'] = ""
+    params['ts'] = ""
+    params['sig'] = ""
+    params['pf'] = ""
+    params['pfkey'] = ""
+    params['zoneid'] = ""
+    params['amt'] = ""
+    params['billno'] = ""
+    cookies = {}
+    cookies['session_id'] = "open_id"
+    cookies['session_id'] = "hy_gameid"
+    cookies['session_type'] = "kp_actoken"
+    cookies['session_type'] = "wc_actoken"
+    cookies['org_loc'] = '/mpay/get_balance_m'
+    for k, v in cookies.items():
+        cookies[k] = urlencode(v)
+    sig = gen_sign(params, sig_uri, "")
+    params['sig'] = sig
+    url += uri
+    async with aiohttp.ClientSession(cookie=cookies) as session:
+        rst = await session.get(url, params=params)
+        ret = await rst.json()
+        status = int(ret['ret'])
+        if status != 0 and status != 1004:
+            print('扣除游戏币失败\n', ret)
+        return json(ret)
+
 
 @app.get('/wzgj/cancel_pay_m')
 async def cancel_pay_m(req):
     "取消支付接口"
-    return json({})
+    # https://ysdk.qq.com/mpay/cancel_pay_m
+    # https://ysdktest.qq.com/mpay/cancel_pay_m
+    d = req.args['param']
+    url = 'https://ysdktest.qq.com'
+    uri = "/mpay/cancel_pay_m "
+    sig_uri = '/v3/r'
+    params = {}
+    params['openid'] = ""
+    params['openkey'] = ""
+    params['appid'] = ""
+    params['ts'] = ""
+    params['sig'] = ""
+    params['pf'] = ""
+    params['pfkey'] = ""
+    params['zoneid'] = ""
+    params['amt'] = ""
+    params['billno'] = ""
+    cookies = {}
+    cookies['session_id'] = "open_id"
+    cookies['session_id'] = "hy_gameid"
+    cookies['session_type'] = "kp_actoken"
+    cookies['session_type'] = "wc_actoken"
+    cookies['org_loc'] = '/mpay/get_balance_m'
+    for k, v in cookies.items():
+        cookies[k] = urlencode(v)
+    sig = gen_sign(params, sig_uri, "")
+    params['sig'] = sig
+    url += uri
+    async with aiohttp.ClientSession(cookie=cookies) as session:
+        rst = await session.get(url, params=params)
+        ret = await rst.json()
+        status = int(ret['ret'])
+        if status != 0:
+            print('取消支付失败\n', ret)
+        return json(ret)
+
 
 @app.get('/wzgj/present_m')
 async def present_m(req):
     "直接赠送接口"
-    return json({})
+    # https://ysdk.qq.com/mpay/present_m
+    # https://ysdktest.qq.com/mpay/present_m
+    d = req.args['param']
+    url = 'https://ysdktest.qq.com'
+    uri = "/mpay/present_m"
+    sig_uri = '/v3/r'
+    params = {}
+    params['openid'] = ""
+    params['openkey'] = ""
+    params['appid'] = ""
+    params['ts'] = ""
+    params['sig'] = ""
+    params['pf'] = ""
+    params['pfkey'] = ""
+    params['zoneid'] = ""
+    params['presenttimes'] = ""
+    params['billno'] = ""
+    cookies = {}
+    cookies['session_id'] = "open_id"
+    cookies['session_id'] = "hy_gameid"
+    cookies['session_type'] = "kp_actoken"
+    cookies['session_type'] = "wc_actoken"
+    cookies['org_loc'] = '/mpay/get_balance_m'
+    for k, v in cookies.items():
+        cookies[k] = urlencode(v)
+    sig = gen_sign(params, sig_uri, "")
+    params['sig'] = sig
+    url += uri
+    async with aiohttp.ClientSession(cookie=cookies) as session:
+        rst = await session.get(url, params=params)
+        ret = await rst.json()
+        status = int(ret['ret'])
+        if status != 0:
+            print('赠送游戏币失败\n', ret)
+        return json(ret)
+
 
 def gen_sign(data, uri, appkey='', method='GET'):
     "OpenAPI V3.0"
@@ -208,11 +338,10 @@ def gen_sign(data, uri, appkey='', method='GET'):
     s2 = urlencode(s2).upper()
     ret = "&".join([method, s1, s2])
     key = appkey + "&"
-    sig = urlencode(b64encode(hmac.new(key.encode(), ret.encode(), 'sha1').digest()))
+    sig = urlencode(
+        b64encode(hmac.new(key.encode(), ret.encode(), 'sha1').digest()))
     return sig
 
 
-
-# 192.168.1.249:8000/user/123
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
